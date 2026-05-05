b7ffa9241fe9e120bfe4256db30cdc0e0df55ec0
Debangsu-Malakar <debmalakar1984@gmail.com>
2026-05-05 17:46:57 +0530

    updated problem1

--- Problem1.java
+++ Problem1.java
@@ -1,4 +1,4 @@
-public class Problem_One {
+public class PROBLEM1 {
     public static void main(String[] args) {
         String s = "abca";
         int n = s.length();
@@ -6,20 +6,20 @@ public class Problem_One {
 
         String doubled = s + s; //doubled it so we can check cycle cases...
 
-        // Outer loop → choose starting index of substring
+       
         for (int i = 0; i < n; i++) {
             boolean[] seen = new boolean[26];// boolean array where we store true value if seen ...
             int sum = 0;
 
-            // Inner loop (expand substring from index i)
+           
             for (int j = i; j < i + n; j++) {
                 char ch = doubled.charAt(j);
 
-                //If character already seen i.e(duplicate)...stop this substring
+                
                 if (seen[ch - 'a']) {
                     break;
                 }
-                //  marks character as seen
+               
                 seen[ch - 'a'] = true;
                 sum += (ch - 'a' + 1);
                 maxSum = Math.max(maxSum, sum);
